6aa4c182b793f4920720e392aa59fd30137e7b09
Ethan Ky <eky2429@gmail.com>
2026-09-24 09:53:50 -0500

    Updated pages of the website

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,1 +1,14 @@
 //Code for projects page
+
+//Get the feature projects container
+var featuredProjectsSection = document.getElementById("featured-projects");
+
+//Get the list of featured projects
+var featuredProjects = document.getElementsByClassName("featured");
+
+//Add projects to the container
+if (featuredProjectsSection && featuredProjects.length) {
+  Array.from(featuredProjects).forEach(function (project) {
+    featuredProjectsSection.appendChild(project);
+  });
+}
